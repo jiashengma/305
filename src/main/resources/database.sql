48e428f3f67d735fb0c7365a45fcb1c70899aae5
jiashengma <jiasheng.ma@stonybrook.edu>
2017-09-19 17:09:37 -0400

    updated tables

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -3,52 +3,58 @@
 -- TODO: keys, constraints
 CREATE TABLE airline (
     id      INTEGER,
-    name    CHAR(2),
+    name    CHAR(2) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE airport (
     id      INTEGER,
-    name    CHAR(3),
-    city    VARCHAR(64),
-    country VARCHAR(64),
+    name    CHAR(3)  NOT NULL,
+    city    VARCHAR(64) NOT NULL,
+    country VARCHAR(64) NOT NULL,
     PRIMARY KEY (id)
 );
 
 -- TODO
 CREATE TABLE flight (
     flightNumber    INTEGER,
-    aid             INTEGER, -- airline id
+    aid             CHAR(2), -- airline id
     numSeat         INTEGER,
     -- ENUM('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
     fare            FLOAT(10,2),
-    hiddenFare      FLOAT(10,2), -- lowest bid the airline is willing to accept
+    -- lowest bid the airline is willing to accept
+    hiddenFare      FLOAT(10,2) NOT NULL, 
     -- TODO: fare restrictions?
-    PRIMARY KEY (flightNumber),
+    PRIMARY KEY (flightNumber, aid),
     FOREIGN KEY (aid) REFERENCES airline(id)
 );
 
 CREATE TABLE stops (
-    aid             INTEGER, -- airline id
-    flightNumber    INTEGER,
-    departureCity
-    departureCountry
-    departureTime
-    arrivalCity
-    arrivalCountry
-    arrivalTime
-    
-    --TODO: primary key?
-    FOREIGN KEY (aid) REFERENCES airline(id,
+    id              INTEGER,    -- stop number
+    aid             INTEGER NOT NULL,  -- airline id
+    flightNumber    INTEGER NOT NULL,
+    /* TODO: can we use airports instead of citys as stops? b/c flight stops
+            at airport
+    */ 
+    fromAirport     INTEGER NOT NULL,
+    toAirport       INTEGER NOT NULL,
+    departureTime   DATETIME-- TODO: type?
+    arrivalTime     -- TODO: type?
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (aid) REFERENCES airline(id),
     FOREIGN KEY (flightNumber) REFERENCES flight
-    
+    FOREIGN KEY (fromAirport) REFERENCES airport(id),
+    FOREIGN KEY (toAirport) REFERENCES airport(id),
+
 );
 
 -- flight operates on what day(s)
 CREATE TABLE operatesOn (
     flightNumber    INTEGER,
+    aid             INTEGER,
     day             INTEGER(1),
-    PRIMARY KEY (flightNumber, day)
+    PRIMARY KEY (flightNumber, aid, day)
 );
 
 CREATE TABLE reservation (
@@ -60,67 +66,78 @@ CREATE TABLE reservation (
     eid         INTEGER, -- employee (customer representative) id
     
     PRIMARY KEY (reservationNumber),
-    -- TODO: create an id for customer representative or use ssn
+    -- TODO: create an id for customer representative or use ssn?
     FOREIGN KEY (eid) REFERENCES employee(ssn)
 );
 
 CREATE TABLE leg (
     reservationNumber   INTEGER,
-    fromAirport         INTEGER,
-    toAirport           INTEGER,
-    flightNumber        INTEGER,
-    aid                 INTEGER
+    fromAirport     INTEGER,
+    toAirport       INTEGER,
+    flightNumber    INTEGER,
+    aid             INTEGER,
+    seatNumber      INTEGER,
     departureTime   TIMESTAMP,
     arrivalTime     TIMESTAMP,
-    class       ENUM('Economy','Business','First'),
-    seatNumber  INTEGER,
+    meal            VARCHAR(32), -- TODO: USE ENUM?
+    class          ENUM('Economy','Business','First'),
 
     FOREIGN KEY (reservationNumber) REFERENCES reservation,
     FOREIGN KEY (fromAirport) REFERENCES airport(id),
     FOREIGN KEY (toAirport) REFERENCES airport(id),
     FOREIGN KEY (aid) REFERENCES airline(id)
-    
+);
+
+-- TODO: fare restrictions
+
+CREATE TABLE person (
+    id          INTEGER,
+    lastname    VARCHAR(32) NOT NULL,
+    firstname   VARCHAR(32) NOT NULL,
+    address     VARCHAR(64) NOT NULL,
+    city        VARCHAR(32) NOT NULL,
+    state       CHAR(2) NOT NULL,
+    zipcode     INTEGER(5) NOT NULL,
+    phone       CHAR() NOT NULL, -- TODO: CHECK WHAT FORMAT
+    email       VARCHAR(64) NOT NULL,
+
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE customer (
-    lastname    VARCHAR(32),
-    firstname   VARCHAR(32),
-    address     VARCHAR(64),
-    city        VARCHAR(32),
-    state       CHAR(2),
-    zipcode     INTEGER(5),
-    phone       INTEGER(9), -- TODO: CHECK
-    email       VARCHAR(64),
-    account     INTEGER,    -- account number
-    accountCreationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    creditCardNum   -- TODO
-    -- TODO: preferences -> from another table
-    rating      --TODO: type?
-    
-    PRIMARY KEY(account)
+    id          INTEGER UNIQUE NOT NULL,
+    account     INTEGER NOT NULL,    -- account number
+    accountCreationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    creditCardNum   BIGINT NOT NULL, 
+    rating      INTEGER DEFAULT 0, -- each ticket transcation rating should +1
+
+    PRIMARY KEY(account),
+    FOREIGN KEY (id) REFERENCES person
 
 );
 
--- who reserves 
+-- who reserves what 
 CREATE TABLE reserves (
-    cid                 INTEGER, -- customer
+    customer            INTEGER, -- customer account number
     reservationNumber   INTEGER,
 
     PRIMARY KEY (cid, reservationNumber)
 );
 
-CREATE TABLE employee(
-    ssn         INTEGER,
-    lastname    VARCHAR(32),
-    firstname   VARCHAR(32),
-    address     VARCHAR(64),
-    city        VARCHAR(32),
-    state       CHAR(2),
-    zipcode     INTEGER(5),
-    phone       INTEGER(9), -- TODO: CHECK
-
-    startDate   DATE , -- TODO: DEFAULT DATE ON CREATE?
-    hourlyRate  FLOAT(3,2),
-    PRIMARY KEY(ssn)
+-- customer's preferene 
+CREATE TABLE preference (
+    customer    INTEGER, -- customer account number
+    seat        ENUM('Aisle Seat', 'Window Seat'),
+    meal        ENUM('Meal 1', 'Meal 2'), -- TODO: check meal options \
+    FOREIGN KEY (customer)
 );
 
+CREATE TABLE employee(
+    id          INTEGER UNIQUE NOT NULL,
+    ssn         CHAR(12),
+    isManager   TINYINT DEFUALT 0 NOT NULL, -- customer representative
+    startDate   DATE , -- TODO: DEFAULT DATE ON CREATE?
+    hourlyRate  FLOAT(5,2),
+    PRIMARY KEY(ssn),
+    FOREIGN KEY (id) REFERENCES person
+);
