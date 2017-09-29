6637881c64d67ef632b0262cdb4486c118d7c4f1
Antonio Aliberti <antonio.aliberti@stonybrook.edu>
2017-09-28 21:01:38 -0400

    Fix typos; merge reserves with reservation This also fixes some data type issues (i.e., foreign keys referencing mismatching data types). One FOREIGN KEY has been commented out (eid/ssn) temporarily. Some entries have also been assigned some data types.

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -18,7 +18,7 @@ CREATE TABLE airport (
 -- TODO
 CREATE TABLE flight (
     flightNumber    INTEGER,
-    aid             CHAR(2), -- airline id
+    aid             INTEGER, -- airline id
     numSeat         INTEGER,
     -- ENUM('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
     fare            FLOAT(10,2),
@@ -38,14 +38,13 @@ CREATE TABLE stops (
     */ 
     fromAirport     INTEGER NOT NULL,
     toAirport       INTEGER NOT NULL,
-    departureTime   DATETIME-- TODO: type?
-    arrivalTime     -- TODO: type?
+    departureTime   DATETIME,-- TODO: type?
+    arrivalTime     DATETIME,-- TODO: type?
 
     PRIMARY KEY (id),
-    FOREIGN KEY (aid) REFERENCES airline(id),
-    FOREIGN KEY (flightNumber) REFERENCES flight
+    FOREIGN KEY (flightNumber,aid) REFERENCES flight(flightNumber,aid),
     FOREIGN KEY (fromAirport) REFERENCES airport(id),
-    FOREIGN KEY (toAirport) REFERENCES airport(id),
+    FOREIGN KEY (toAirport) REFERENCES airport(id)
 
 );
 
@@ -57,17 +56,63 @@ CREATE TABLE operatesOn (
     PRIMARY KEY (flightNumber, aid, day)
 );
 
+CREATE TABLE person (
+    id          INTEGER,
+    lastname    VARCHAR(32) NOT NULL,
+    firstname   VARCHAR(32) NOT NULL,
+    address     VARCHAR(64) NOT NULL,
+    city        VARCHAR(32) NOT NULL,
+    state       CHAR(2) NOT NULL,
+    zipcode     INTEGER(5) NOT NULL,
+    phone       CHAR(10) NOT NULL, -- TODO: CHECK WHAT FORMAT
+    email       VARCHAR(64) NOT NULL,
+
+    PRIMARY KEY (id)
+);
+CREATE TABLE customer (
+    id          INTEGER UNIQUE NOT NULL,
+    account     INTEGER NOT NULL,    -- account number
+    accountCreationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    creditCardNum   BIGINT NOT NULL, 
+    rating      INTEGER DEFAULT 0, -- each ticket transcation rating should +1
+
+    PRIMARY KEY(account),
+    FOREIGN KEY (id) REFERENCES person(id)
+
+);
+
+
+CREATE TABLE employee(
+    id          INTEGER UNIQUE NOT NULL,
+    ssn         CHAR(12),
+    isManager   TINYINT DEFAULT 0 NOT NULL, -- customer representative
+    startDate   DATE , -- TODO: DEFAULT DATE ON CREATE?
+    hourlyRate  FLOAT(5,2),
+    PRIMARY KEY(ssn),
+    FOREIGN KEY (id) REFERENCES person(id)
+);
+-- customer's preferene 
+CREATE TABLE preference (
+    customer    INTEGER, -- customer account number
+    seat        ENUM('Aisle Seat', 'Window Seat'),
+    meal        ENUM('Meal 1', 'Meal 2'), -- TODO: check meal options \
+    FOREIGN KEY (customer) REFERENCES customer(account)
+);
 CREATE TABLE reservation (
     reservationNumber      INTEGER, 
     -- TODO: set default to current date on create 
     reservationDate   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     totalFare   FLOAT(10,2),
     bookingFee  FLOAT(10,2),
+    seatCount   INTEGER NOT NULL DEFAULT 1,
+    customer    INTEGER NOT NULL,
     eid         INTEGER, -- employee (customer representative) id
     
     PRIMARY KEY (reservationNumber),
     -- TODO: create an id for customer representative or use ssn?
-    FOREIGN KEY (eid) REFERENCES employee(ssn)
+    -- FOREIGN KEY (eid) REFERENCES employee(ssn),
+    -- ^ eid & ssn types don't match.
+    FOREIGN KEY (customer) REFERENCES customer (account)
 );
 
 CREATE TABLE leg (
@@ -82,62 +127,10 @@ CREATE TABLE leg (
     meal            VARCHAR(32), -- TODO: USE ENUM?
     class          ENUM('Economy','Business','First'),
 
-    FOREIGN KEY (reservationNumber) REFERENCES reservation,
+    FOREIGN KEY (reservationNumber) REFERENCES reservation(reservationNumber),
     FOREIGN KEY (fromAirport) REFERENCES airport(id),
     FOREIGN KEY (toAirport) REFERENCES airport(id),
     FOREIGN KEY (aid) REFERENCES airline(id)
 );
 
 -- TODO: fare restrictions
-
-CREATE TABLE person (
-    id          INTEGER,
-    lastname    VARCHAR(32) NOT NULL,
-    firstname   VARCHAR(32) NOT NULL,
-    address     VARCHAR(64) NOT NULL,
-    city        VARCHAR(32) NOT NULL,
-    state       CHAR(2) NOT NULL,
-    zipcode     INTEGER(5) NOT NULL,
-    phone       CHAR() NOT NULL, -- TODO: CHECK WHAT FORMAT
-    email       VARCHAR(64) NOT NULL,
-
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE customer (
-    id          INTEGER UNIQUE NOT NULL,
-    account     INTEGER NOT NULL,    -- account number
-    accountCreationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    creditCardNum   BIGINT NOT NULL, 
-    rating      INTEGER DEFAULT 0, -- each ticket transcation rating should +1
-
-    PRIMARY KEY(account),
-    FOREIGN KEY (id) REFERENCES person
-
-);
-
--- who reserves what 
-CREATE TABLE reserves (
-    customer            INTEGER, -- customer account number
-    reservationNumber   INTEGER,
-
-    PRIMARY KEY (cid, reservationNumber)
-);
-
--- customer's preferene 
-CREATE TABLE preference (
-    customer    INTEGER, -- customer account number
-    seat        ENUM('Aisle Seat', 'Window Seat'),
-    meal        ENUM('Meal 1', 'Meal 2'), -- TODO: check meal options \
-    FOREIGN KEY (customer)
-);
-
-CREATE TABLE employee(
-    id          INTEGER UNIQUE NOT NULL,
-    ssn         CHAR(12),
-    isManager   TINYINT DEFUALT 0 NOT NULL, -- customer representative
-    startDate   DATE , -- TODO: DEFAULT DATE ON CREATE?
-    hourlyRate  FLOAT(5,2),
-    PRIMARY KEY(ssn),
-    FOREIGN KEY (id) REFERENCES person
-);
