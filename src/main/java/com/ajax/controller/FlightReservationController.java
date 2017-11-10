0467eb4c93897f779d542fad42858d02a208d83f
Andrew <andrew.lam@stonybrook.edu>
2017-11-09 19:49:27 -0500

    working on controller for search

--- src/main/java/com/ajax/controller/FlightReservationController.java
+++ src/main/java/com/ajax/controller/FlightReservationController.java
@@ -7,15 +7,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Map;
+
 @Controller
 @ControllerAdvice
 public class FlightReservationController {
     @RequestMapping(value = "/search", method = RequestMethod.GET)
     public ModelAndView searchFlight(HttpServletRequest request) {
         // TODO: please add view(URL)
-        ModelAndView mv = new ModelAndView("");
+        ModelAndView mv = new ModelAndView("/SearchResult");
 
-        
-        return mv;
+        for (Enumeration s = request.getParameterNames(); request.getParameterNames().hasMoreElements();)
+	        System.out.println(s.nextElement());
+
+//	    for (Map.Entry : request.getParameterMap())
+
+//	    try {
+//		    request.getReader().lines().forEach(System.out::println);
+//
+//	    } catch (IOException e) {
+//		    e.printStackTrace();
+//	    }
+
+	    return mv;
     }
 }
