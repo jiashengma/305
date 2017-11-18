f3e53e3a7cef210f4c3b37bb1d577e59c94e6c36
jiashengma <jiasheng.ma@stonybrook.edu>
2017-11-18 11:40:37 -0500

    load airports when it is not stored in session

--- src/main/java/com/ajax/controller/FlightReservationController.java
+++ src/main/java/com/ajax/controller/FlightReservationController.java
@@ -1,4 +1,5 @@
 package com.ajax.controller;
+
 import com.ajax.model.Airport;
 import com.ajax.model.Flight;
 import com.ajax.model.FlightSearchForm;
@@ -20,17 +21,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @ControllerAdvice
 public class FlightReservationController {
+
     @Autowired
     FlightReservationService flightReservationService;
-    
+
     /**
      *
      * @param binder
@@ -44,11 +44,15 @@ public class FlightReservationController {
         //binder.registerCustomEditor(Date.class, "dateAcquired" ,new CustomDateEditor(simpleDateFormat, false));
     }
 
-	@ModelAttribute
-	public void init(HttpServletRequest request) {
-		List<Airport> airports = flightReservationService.getAirports();
-    	request.getSession().setAttribute("s_airports", airports);
-	}
+    @ModelAttribute
+    public void init(HttpServletRequest request) {
+        // load airports only when it is not stored in session 
+        if (request.getSession().getAttribute("s_airports") == null) {
+            List<Airport> airports = flightReservationService.getAirports();
+            request.getSession().setAttribute("s_airports", airports);
+        }
+
+    }
 
     /**
      *
@@ -67,7 +71,7 @@ public class FlightReservationController {
             return new ModelAndView("index");
         }
 
-	    ArrayList<Flight> flights = (ArrayList<Flight>) flightReservationService.searchFlight(flightSearchForm);
+        ArrayList<Flight> flights = (ArrayList<Flight>) flightReservationService.searchFlight(flightSearchForm);
 
         // add a list of flights as the search result for the view/jsp to render
         mv.addObject("flightSearchResult", flights);
@@ -87,9 +91,9 @@ public class FlightReservationController {
             mv.setView(null);
         } else {
             /* TODO: set view or display message
-                  maybe set up a few return codes from the bookFlight() method
-                  1=success, 0=fail_due_to_full_flight, -1=error of some sort
-            */
+             maybe set up a few return codes from the bookFlight() method
+             1=success, 0=fail_due_to_full_flight, -1=error of some sort
+             */
             // mv.setView("index");
             // mv.addObject("msg", "Failed to book flight");
         }
